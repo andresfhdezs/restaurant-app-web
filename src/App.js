f70122778e2eab1113f331485362de9a28ec20d2
Andres Hernandez <andresfhdezs@gmail.com>
2020-04-27 17:54:39 -0500

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+import Ordenes from './components/paginas/Ordenes';
+import Menu from './components/paginas/Menu';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+import Sidebar from './components/ui/Sidebar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-light">Hola World</h1>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
